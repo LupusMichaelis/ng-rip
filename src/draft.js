ea9216e7c569593f557a7953e680935152c2167d
Mickaël Wolff <m@lupusmic.org>
2019-11-18 16:00:34 +0100

    Attempt to have some tabs

--- src/draft.js
+++ src/draft.js
@@ -5,4 +5,58 @@ angular.module('draftApp', [])
     {
         this.yourName = '';
     })
+    .directive('tabs', () => (
+        { restrict: 'E'
+        , transclude: true
+        , scope:
+            {
+            }
+        , controller: function($scope)
+            {
+                var panes = $scope.panes = [];
+                var tabIndex = $scope.tabIndex = 1;
+
+                this.addPane = (pane) =>
+                {
+                    panes.length || $scope.switchTab(0);
+                    panes.push(pane);
+                }
+
+                $scope.switchTab = (position) =>
+                {
+                    $scope.tabIndex = tabIndex = position;
+                }
+            }
+        , template: `<div class='tabbable'>
+                       <ul class='nav nav-tabs'>
+                         <li ng-repeat='pane in panes'
+                             ng-class='{active:$index == tabIndex}'
+                             >
+                           <span ng-click='switchTab($index)'>{{ pane.title }}</span>
+                         </li>
+                       </ul>
+                       <div ng-transclude
+                            class='tab-content'
+                            />
+                     </div>
+                    `
+        , replace: true
+        })
+    )
+    .directive('pane', () => (
+        { require: '^tabs'
+        , restrict: 'E'
+        , transclude: true
+        , scope:
+            { title: '@'
+            }
+        , link: (scope, element, attrs, tabsController) =>
+            tabsController.addPane(scope)
+        , template: `<div ng-transclude
+                          class='tab-pane'
+                          ng-class='{active: $index == tabIndex}'
+                     />`
+        , replace: true
+        })
+    )
 ;
